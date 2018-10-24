9150878800c446513acbb9d5b876eca241b14ed9
Joshua Lansford <Joshua.Lansford+git@laserlinc.com>
2018-10-24 14:18:57 -0400

    Starting to change object name to gsfilelock instead of filelock.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='FileLock',
+    name='GsFileLock',
     version='0.1.0',
     author='Evan Fosmark',
     author_email='me@evanfosmark.com',
-    packages=['filelock','filelock.test'],
-    url='https://github.com/dmfrey/FileLock',
+    packages=['gsfilelock','gsfilelock.test'],
+    url='https://github.com/JEdward7777/FileLock',
     license='LICENSE.txt',
     description='File locking library',
-    long_description=open('README.txt').read()
+    long_description=open('README.txt').read(),
 )
