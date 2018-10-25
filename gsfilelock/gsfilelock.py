effff95ec47ef7e8bd6e6919c006511b42ff0d8b
Joshua Lansford <Joshua.Lansford+git@laserlinc.com>
2018-10-25 08:33:46 -0400

    Wrong start test.

--- gsfilelock/gsfilelock.py
+++ gsfilelock/gsfilelock.py
@@ -51,7 +51,7 @@ class GsFileLock(object):
         """
         self.is_locked = False
         self.consistency_time = consistency_time
-        if file_name.startswith( "gs: " ):
+        if file_name.startswith( "gs:/" ):
             self.lockfile = file_name
         else:
             self.lockfile = os.path.join(os.getcwd(), "%s.lock" % file_name)
